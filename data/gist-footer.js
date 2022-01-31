0f6d3914b16cb6d7d57e4942412e4ca949d7b5cb
robert hudek <hudektech@gmail.com>
2022-01-31 15:46:02 +0800

    3

--- data/gist-footer.js
+++ data/gist-footer.js
@@ -1,5 +1,4 @@
 // handle id
-
 const atags = [...document.querySelectorAll('a[id^="user-content"]')];
 
 for (let atag of atags) {
@@ -7,17 +6,25 @@ for (let atag of atags) {
   parent.id = atag.getAttribute("href").substring(1);
 }
 
+// add in print button
+let h1 = document.querySelector("#online-help");
+h1.innerHTML += `  <button class="my-btn" onclick="window.print();return false;">
+    <i class="fas fa-print"></i> Print
+  </button>`;
+
+h1.classList.add("my-btn-flex");
+
 // handle footer
-footer = document.querySelector(".gist-meta");
-tags = [...footer.querySelectorAll("a")];
+let footer = document.querySelector(".gist-meta");
+let tags = [...footer.querySelectorAll("a")];
 
-arr = tags[0].href.split("gist.github.com/");
-arr2 = arr[1].split("/");
+let arr = tags[0].href.split("gist.github.com/");
+let arr2 = arr[1].split("/");
 
-myAccount = arr2[0];
-myAccountHref = `https://github.com/${myAccount}`;
-myGist = tags[1].innerText;
-myGistHref = tags[1].href;
+let myAccount = arr2[0];
+let myAccountHref = `https://github.com/${myAccount}`;
+let myGist = tags[1].innerText;
+let myGistHref = tags[1].href;
 
 // Update footer
 footer.innerHTML = `<a href="${myGistHref}">${myGist}</a> made with ❤ by <a href="${myAccountHref}">${myAccount}</a>`;
